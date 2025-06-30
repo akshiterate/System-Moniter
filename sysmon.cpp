f3af5a1b1051fad732d2e4bb06d9a1ead6455448
Akshit Sachdeva <akshitsac@email.com>
2025-06-30 20:40:53 +0000

    added usage

--- sysmon.cpp
+++ sysmon.cpp
@@ -2,11 +2,26 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <unistd.h>
 using namespace std;
 
-int main(){
+class Stats{
+public:
+    long user,nice,system,idle,iowait,irq,softirq;
+
+    long total() const{
+       return user + nice + system + idle + iowait + irq + softirq;
+    }
+
+    long idleTime() const {
+        return idle + iowait;
+    }
+};
+
+Stats read(){
     ifstream statFile("/proc/stat");
     string line;
+    Stats stats;
     if(statFile .is_open()){
         getline(statFile,line);
 //        cout<<line;
@@ -14,11 +29,22 @@ int main(){
     else{
         cout<<"gay";
     }
-    string label,user,nice,system,idle,iowait,irq,softirq,steal,guest,guest_nice;
     istringstream iss(line);
-    iss >> label >> user >>  nice >> system >> idle >> iowait >> irq >> softirq >> steal >> guest >> guest_nice;
-    cout<<"Label:"<<label<<endl;;
-    cout<<"user:"<<user<<endl;
-    cout<<"idle:"<<idle;
-    return 0;
+    string label;
+    
+    iss >> label >> stats.user >>  stats.nice >> stats.system >> stats.idle >> stats.iowait >> stats.irq >> stats.softirq;
+    return stats;
+}
+
+int main(){
+    Stats s1 = read();
+    sleep(1);
+    Stats s2 = read();
+    long deltaTotal = s2.total() - s1.total();
+    long deltaIdle  = s2.idleTime() - s1.idleTime();
+
+    
+    float cpuUsage = (deltaTotal - deltaIdle) / (float)deltaTotal * 100.0;
+    cout<<"Usage: "<<cpuUsage<<"%"<<endl;
+
 }
